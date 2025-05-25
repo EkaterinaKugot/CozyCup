9890284893bf622ccc797c7e362ee07dd38ec438
EkaterinaKugot <kali.kugot@yandex.ru>
2025-05-25 21:13:57 +0800

    Added pause of training video on the phone

--- src/scripts/game_elements/video_rect.gd
+++ src/scripts/game_elements/video_rect.gd
@@ -1,8 +1,17 @@
 extends TextureRect
 
 @onready var video_stream_player: VideoStreamPlayer = $"../../SubViewport/VideoStreamPlayer"
+var video_pressed: bool = false
 
 func _input(event):
-	if (event is InputEventScreenTouch and event.pressed) or \
-	(event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
+	if OS.get_name() == "Android" or OS.get_name() == "iOS":
+		if event is InputEventScreenTouch and event.pressed:
+			video_pressed = true
+	else:
+		if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			video_pressed = true
+	
+	if video_pressed: 
+		video_pressed = false
 		video_stream_player.paused = not video_stream_player.paused
+		
